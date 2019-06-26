1ecedced33acf9bd2c79f0b24c3fcf132b131de9
ericbowman <ericjbowman@gmail.com>
2019-06-26 14:33:51 -0400

    Add ui.js
    @agregthomas

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -1,3 +1,8 @@
+'use strict'
+
+const config = require('./config')
+const store = require('./store')
+
 const signUp = function (data) {
   return $.ajax({
     url: config.apiUrl + '/sign-up',
--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,5 +1,7 @@
-const getFormFields = require(`../../lib/get-form-fields`)
 const api = require('./api')
+const getFormFields = require(`../../lib/get-form-fields`)
+const store = require('./store')
+const ui = require('./ui')
 
 const onSignUp = function (event) {
   event.preventDefault()
@@ -13,7 +15,7 @@ const onSignIn = function (event) {
   event.preventDefault()
   const data = getFormFields(this)
   api.signIn(data)
-    // .then(ui.signInSuccess)
+    .then(ui.signInSuccess)
     // .catch(ui.signInFailure)
 }
 
@@ -28,7 +30,7 @@ const onChangePassword = function (event) {
   event.preventDefault()
   const data = getFormFields(this)
   api.changePassword(data)
-    // .then(ui.changePasswordSuccess)
+    .then(() => console.log(store.user))
     // .catch(ui.changePasswordFailure)
 }
 
@@ -36,6 +38,7 @@ const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
   $('#sign-out').on('submit', onSignOut)
+  $('#change-password').on('submit', onChangePassword)
 }
 
 module.exports = {
