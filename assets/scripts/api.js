ba720b1d00fbb4230d6a6b9335b94ba0df92b8a6
tohearne <thomas.ohearne@yahoo.com>
2019-06-27 10:41:44 -0400

    Create events for create survey (and options)
    - Fixed option input names to be plural
    - Added onCreateSurvey to events
      - onCreateSurvey creates a new survey and then creates each option
      - on failure at any point the intent is that it will delete anything
        it has already made so far, this failure should not happen but I
        added it just in case.
    - Added createSurvey and deleteSurvey to api
    - Added createOption and deleteOption to api

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -40,9 +40,55 @@ const changePassword = function (data) {
   })
 }
 
+const createSurvey = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/surveys',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const deleteSurvey = function (id) {
+  return $.ajax({
+    url: config.apiUrl + '/surveys/' + id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const createOption = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/options',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const deleteOption = function (id) {
+  return $.ajax({
+    url: config.apiUrl + '/options/' + id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
 module.exports = {
   signUp,
   signIn,
   signOut,
-  changePassword
+  changePassword,
+  createSurvey,
+  deleteSurvey,
+  createOption,
+  deleteOption
 }
--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -34,11 +34,35 @@ const onChangePassword = function (event) {
     // .catch(ui.changePasswordFailure)
 }
 
+const onCreateSurvey = async function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  let newSurvey
+  const newOptions = []
+  try {
+    newSurvey = await api.createSurvey({ survey: data.survey })
+    console.log(newSurvey.survey._id)
+    for (let i = 0; i < data.options.length; i++) {
+      newOptions[i] = await api.createOption({option: {name: data.options[i], survey: newSurvey.survey._id}})
+    }
+  } catch (err) {
+    console.log(newOptions)
+    if (newSurvey) {
+      api.deleteSurvey(newSurvey.survey.id)
+      newOptions.forEach(option => api.deleteOption(option.option._id))
+    }
+    // ui.createSurveyFailure
+    throw err
+  }
+}
+
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
   $('#sign-out').on('submit', onSignOut)
   $('#change-password').on('submit', onChangePassword)
+
+  $('#create-survey').on('submit', onCreateSurvey)
 
   store.optionCount = 0
   $('#add-option').on('click', ui.onAddOption)
