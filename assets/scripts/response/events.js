d1c7b5887b3f63891a421869c0ddc208a9d3e620
cande04 <cory.anders.atc@gmail.com>
2019-06-28 17:11:55 -0400

    Fix API call for no response
    1) prevent api call if user does not select answer

--- assets/scripts/response/events.js
+++ assets/scripts/response/events.js
@@ -10,15 +10,16 @@ const onCreateResponse = function (event) {
   const optionId = $(`form[data-id='${surveyId}'] input:checked`).data('id')
 
   const responseData = { response: { answer: optionId } }
-
-  api.createResponse(responseData)
-    .then((res) => {
-      surveyEvent.onIndexAfterSubmit(event)
-    })
-    // .then(() => $(`.messaging-${surveyId}`).html('thanks for submitting!'))
-
-    .catch($(`.messaging-${surveyId}`).html('pick something!'))
-    .catch(console.error)
+  if (responseData.response.answer) {
+    api.createResponse(responseData)
+      .then((res) => {
+        surveyEvent.onIndexAfterSubmit(event)
+      })
+      // .then(() => $(`.messaging-${surveyId}`).html('thanks for submitting!'))
+      .catch(console.error)
+  } else {
+    $(`.messaging-${surveyId}`).html('pick something!')
+  }
 }
 
 module.exports = {
