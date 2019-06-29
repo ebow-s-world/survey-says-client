82d69958528ba960284d76321e5316ada839b738
cande04 <cory.anders.atc@gmail.com>
2019-06-29 17:53:34 -0400

    fix rebase conflicts

--- assets/scripts/response/events.js
+++ assets/scripts/response/events.js
@@ -11,25 +11,17 @@ const onCreateResponse = function (event) {
   const surveyId = $(event.target.parentElement).data('id')
   const optionId = $(`form[data-id='${surveyId}'] input:checked`).data('id')
 
-<<<<<<< HEAD
-  const responseData = { response: { answer: optionId } }
-=======
   const responseData = { response: { answer: optionId, survey: surveyId } }
   store.responses = { survey: surveyId, response: responseData.response.answer }
 
->>>>>>> Add limit for submitting Responses
   if (responseData.response.answer) {
     api.createResponse(responseData)
       .then((res) => {
         surveyEvent.onIndexAfterSubmit(event)
       })
       // .then(() => $(`.messaging-${surveyId}`).html('thanks for submitting!'))
-<<<<<<< HEAD
-      .catch(console.error)
-=======
       .catch(responseUi.onSubmitFailure)
       // .catch(console.error)
->>>>>>> Add limit for submitting Responses
   } else {
     $(`.messaging-${surveyId}`).html('pick something!')
   }
