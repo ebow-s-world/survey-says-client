8011a338f218c19f9451b8e94b59c543bcb9bc21
ericbowman <ericjbowman@gmail.com>
2019-06-29 17:02:58 -0400

    Make character limit accommodate board

--- assets/scripts/survey/events.js
+++ assets/scripts/survey/events.js
@@ -99,7 +99,6 @@ const onUpdateSurvey = async function (event) {
 const onGetResults = (event) => {
   event.preventDefault()
   const surveyId = $(event.target.parentElement.parentElement).data('id')
-  console.log('surveyID is', surveyId)
   $(`.results-${surveyId}`).toggleClass('disable')
 }
 
